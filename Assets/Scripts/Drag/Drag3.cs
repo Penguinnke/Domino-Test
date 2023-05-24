1eb117b780792cbfe1789c4e618b58b1eea43f62
Penguinnke <penguinnke2801@gmail.com>
2023-05-24 21:50:34 +0200

    Probeer ervoor te zorgen dat de blokjes elkaar iets eerder herkennen

--- Assets/Scripts/Drag/Drag3.cs
+++ Assets/Scripts/Drag/Drag3.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Drag3 : MonoBehaviour
 {
@@ -8,10 +9,24 @@ public class Drag3 : MonoBehaviour
 
     private Vector2 _offset, _orginalPosition;
 
-    public Transform targetObject; // Reference to the object you want to stick to
+    // [SerializeField]
+    // public List<Transform> targetObject;
+
+    private GameObject targetTransform;
+    private Transform targetObject;
+
+    //public Transform targetObject; // Reference to the object you want to stick to
+    
     public float stickDistance = 1f; // Distance from the target object's side
 
     public string _dominoTag = "Domino1";
+
+    private void Start() 
+    {
+        targetTransform = GameObject.FindGameObjectWithTag(_dominoTag);
+        targetObject = targetTransform.GetComponent<Transform>();
+
+    }
 
     private void Awake() 
     {
@@ -22,6 +37,28 @@ public class Drag3 : MonoBehaviour
         if(!_dragging) return;
         var mousePosition = GetMousePos();
         transform.position = mousePosition - _offset;
+
+        //define the detection area
+        Vector2 center = transform.position;
+        Vector2 size = targetObject.GetComponent<SpriteRenderer>().bounds.size;
+
+        //Check if any colliders overlap with the detection area
+        Collider2D[] colliders = Physics2D.OverlapAreaAll(center - size / 30f, center + size / 30f);
+
+        //Loop through the detected colliders 
+        foreach (Collider2D collider in colliders)
+        {
+            if (collider.gameObject != gameObject && collider.gameObject == targetObject.gameObject)
+            {
+                //Collision with the target block detected
+                Debug.Log("Collision detected with the target");
+                //Perform action here
+                // Calculate the position on the side of the target object
+                Vector2 targetPosition = (Vector2)targetObject.position + (Vector2)targetObject.right * stickDistance;
+                transform.position = Vector2.Lerp(transform.position, targetPosition, Time.deltaTime * 5f);               
+            }
+        }
+
 
     }
 
@@ -42,18 +79,18 @@ public class Drag3 : MonoBehaviour
         return Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 
-    private void OnCollisionEnter2D(Collision2D _collision) 
-    {
-        if (_collision.gameObject.tag == _dominoTag)
-        {
-            Debug.Log("Collision 1!");
-            // Calculate the position on the side of the target object
-            Vector2 targetPosition = (Vector2)targetObject.position + (Vector2)targetObject.right * stickDistance;
-            transform.position = Vector2.Lerp(transform.position, targetPosition, Time.deltaTime * 5f);
-        } else {
-            Debug.Log("No");
-        }
-    } 
+    // private void OnCollisionEnter2D(Collision2D _collision) 
+    // {
+    //     if (_collision.gameObject.tag == _dominoTag)
+    //     {
+    //         Debug.Log("Collision 1!");
+    //         // Calculate the position on the side of the target object
+    //         Vector2 targetPosition = (Vector2)targetObject.position + (Vector2)targetObject.right * stickDistance;
+    //         transform.position = Vector2.Lerp(transform.position, targetPosition, Time.deltaTime * 5f);
+    //     } else {
+    //         Debug.Log("No");
+    //     }
+    // } 
     
 }
 
