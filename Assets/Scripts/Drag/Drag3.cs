752c7deef621e3d8ad3e4175f83c053e50710e89
Penguinnke <penguinnke2801@gmail.com>
2023-05-24 23:00:08 +0200

    Uitprobeersel om de collision field groter te maken

--- Assets/Scripts/Drag/Drag3.cs
+++ Assets/Scripts/Drag/Drag3.cs
@@ -9,13 +9,16 @@ public class Drag3 : MonoBehaviour
 
     private Vector2 _offset, _orginalPosition;
 
-    // [SerializeField]
-    // public List<Transform> targetObject;
+    private GameObject targetTransform; // Reference to the object you want to stick to
+    private Transform targetObject; 
 
-    private GameObject targetTransform;
-    private Transform targetObject;
+    public BoxCollider2D boxCollider2D;
+    public float XSizeBC_A = 5; //before colliding
+    public float YSizeBC_A = 5;
 
-    //public Transform targetObject; // Reference to the object you want to stick to
+    public float XSizeBC_B = 5; //after colliding
+    public float YSizeBC_B = 5;
+
     
     public float stickDistance = 1f; // Distance from the target object's side
 
@@ -26,6 +29,9 @@ public class Drag3 : MonoBehaviour
         targetTransform = GameObject.FindGameObjectWithTag(_dominoTag);
         targetObject = targetTransform.GetComponent<Transform>();
 
+        // Call the ChangeSize method with the desired size
+        Vector2 newSize = new Vector2(XSizeBC_A, YSizeBC_A);
+        ChangeSize(newSize);
     }
 
     private void Awake() 
@@ -50,14 +56,8 @@ public class Drag3 : MonoBehaviour
     {
         _dragging = false;
         _offset = GetMousePos() - (Vector2)transform.position;
-
-        if(Vector2.Distance(transform.position, targetObject.transform.position) < 5)
-        {
-            Vector2 targetPosition = (Vector2)targetObject.position + (Vector2)targetObject.right * stickDistance;
-            transform.position = Vector2.Lerp(transform.position, targetPosition, Time.deltaTime * 5f);
-        } else {
-            Debug.Log("No");
-        }
+        FreezeXY();
+        
     }
 
     Vector2 GetMousePos()
@@ -65,18 +65,42 @@ public class Drag3 : MonoBehaviour
         return Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 
-    // private void OnCollisionEnter2D(Collision2D _collision) 
-    // {
-    //     if (_collision.gameObject.tag == _dominoTag)
-    //     {
-    //         Debug.Log("Collision 1!");
-    //         // Calculate the position on the side of the target object
-    //         Vector2 targetPosition = (Vector2)targetObject.position + (Vector2)targetObject.right * stickDistance;
-    //         transform.position = Vector2.Lerp(transform.position, targetPosition, Time.deltaTime * 5f);
-    //     } else {
-    //         Debug.Log("No");
-    //     }
-    // } 
+    private void OnCollisionEnter2D(Collision2D _collision) 
+    {
+        if (_collision.gameObject.tag == _dominoTag)
+        {
+            Debug.Log("Collision 1!");
+            _dragging = false;
+            CalculatePos(); // Calculate the position on the side of the target object
+            _collision.gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
+        
+            // Call the ChangeSize method with the desired size
+            Vector2 newSize = new Vector2(XSizeBC_B, YSizeBC_B);
+            ChangeSize(newSize);
+        
+        } else {
+            Debug.Log("No");
+            _dragging = false;
+        }
+    } 
+
+    private void FreezeXY()
+    {
+        gameObject.GetComponent<Rigidbody2D>().constraints &= ~RigidbodyConstraints2D.FreezePositionX;
+        gameObject.GetComponent<Rigidbody2D>().constraints &= ~RigidbodyConstraints2D.FreezePositionY;
+    }
+
+    private void CalculatePos()
+    {
+        Vector2 targetPosition = (Vector2)targetObject.position + (Vector2)targetObject.right * stickDistance;
+        transform.position = Vector2.Lerp(transform.position, targetPosition, Time.deltaTime * 5f);
+    }
+
+    private void ChangeSize(Vector2 newSize)
+    {
+        boxCollider2D.size = newSize;
+    }
+    
     
 }
 
