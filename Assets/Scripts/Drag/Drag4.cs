64a55b21b557739033f0417ff43002094fe1dacd
Penguinnke <penguinnke2801@gmail.com>
2023-05-25 14:27:21 +0200

    Domino maken
    Moet ervoor zorgend dat de twee onderdelen aan elkaar blijven plakken

--- Assets/Scripts/Drag/Drag4.cs
+++ Assets/Scripts/Drag/Drag4.cs
@@ -3,11 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
-public class Drag3 : MonoBehaviour
+public class Drag4 : MonoBehaviour
 {
-    private bool _dragging, _placed;
-
-    private Vector2 _offset, _orginalPosition;
 
     private GameObject targetTransform; // Reference to the object you want to stick to
     private Transform targetObject; 
@@ -22,52 +19,17 @@ public class Drag3 : MonoBehaviour
         targetTransform = GameObject.FindGameObjectWithTag(_dominoTag);
         targetObject = targetTransform.GetComponent<Transform>();
     }
-
-    private void Awake() 
-    {
-        _orginalPosition = transform.position;
-    }
-
-    private void Update() {
-        if(!_dragging) return;
-        var mousePosition = GetMousePos();
-        transform.position = mousePosition - _offset;
-
-        if (_dragging == true) 
-        {
-            UnfreezeXY();
-        }
-    }
-
-    private void OnMouseDown()
-    {
-        _dragging = true;
-        _offset = GetMousePos() - (Vector2)transform.position;
-    }
-
-    private void OnMouseUp() 
-    {
-        _dragging = false;
-        _offset = GetMousePos() - (Vector2)transform.position;
-    }
-
-    Vector2 GetMousePos()
-    {
-        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
-    }
-
+    
     private void OnCollisionEnter2D(Collision2D _collision) 
     {
         if (_collision.gameObject.tag == _dominoTag)
         {
             Debug.Log("Collision 1!");
-            _dragging = false;
             CalculatePos(); // Calculate the position on the side of the target object
             _collision.gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
         
         } else {
             Debug.Log("No");
-            _dragging = false;
         }
     } 
 
@@ -75,7 +37,6 @@ public class Drag3 : MonoBehaviour
     {
         gameObject.GetComponent<Rigidbody2D>().constraints &= ~RigidbodyConstraints2D.FreezePositionX;
         gameObject.GetComponent<Rigidbody2D>().constraints &= ~RigidbodyConstraints2D.FreezePositionY;
-        _dragging = true;
     }
 
     private void CalculatePos()
@@ -86,4 +47,3 @@ public class Drag3 : MonoBehaviour
     
     
 }
-
