52b66efbe291c4f7885f7a38cea96252b5fa16c6
Penguinnke <121119907+Penguinnke@users.noreply.github.com>
2023-07-04 18:58:24 +0200

    Colour bug fix

--- Assets/Scripts/Domino/2DPhysicsLeren/NEW/DominoColour.cs
+++ Assets/Scripts/Domino/2DPhysicsLeren/NEW/DominoColour.cs
@@ -2,12 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DominoCollision : MonoBehaviour
+public class DominoColour : MonoBehaviour
 {
     public bool colliding;
     public bool isCollisionDetectedRight;
-
-    public Rigidbody2D rb2d; //Rigidbody from the entire Domino
 
     //Audio
     public AudioSource audiosourcecolliding;
@@ -23,12 +21,21 @@ public class DominoCollision : MonoBehaviour
     public string _dominoTagFALSE4 = "Domino4"; //the domino tag it needs to collide with
     public string _dominoTagFALSE5 = "Domino5"; //the domino tag it needs to collide with
 
+    public Color collisionColor; // The color to apply upon collision
+    public Color wrongCollisionColor;// The color to apply upon collision with a wrong tag
+    public Color exitColor; // The color to apply when the collision ends
+    public float delay = 2f; // The delay in seconds before changing the color back
+    private SpriteRenderer spriteRenderer;
+    private Coroutine colorChangeCoroutine;
+    
     private void Start()
     {
         //isCollisionDetectedRight = false;
         colliding = false;
 
         NotCollidingSound();
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
     
     private void OnCollisionEnter2D(Collision2D _collision)
@@ -38,45 +45,57 @@ public class DominoCollision : MonoBehaviour
             Debug.Log("Collision!");
             colliding = true;
             isCollisionDetectedRight = true;
+            spriteRenderer.color = collisionColor;
+
+            if (colorChangeCoroutine != null)
+            {
+                StopCoroutine(colorChangeCoroutine);
+            }
+            colorChangeCoroutine = StartCoroutine(ChangeColorWithDelay(exitColor, delay));
         }
 
         if(_collision.gameObject.tag == _dominoTagFALSE1)
 
         {
-            Debug.Log("Not Collision!");
+            Debug.LogError("WRONG COLLISION");
             colliding = false;
             isCollisionDetectedRight = false;
+            spriteRenderer.color = wrongCollisionColor;
         }
 
         if(_collision.gameObject.tag == _dominoTagFALSE2)
         {
-            Debug.Log("Not Collision!");
+            Debug.LogError("WRONG COLLISION");
             colliding = false;
             isCollisionDetectedRight = false;
+            spriteRenderer.color = wrongCollisionColor;
 
         }
 
         if(_collision.gameObject.tag == _dominoTagFALSE3)
         {
-            Debug.Log("Not Collision!");
+            Debug.LogError("WRONG COLLISION");
             colliding = false;
             isCollisionDetectedRight = false;
+            spriteRenderer.color = wrongCollisionColor;
 
         }
 
         
         if(_collision.gameObject.tag == _dominoTagFALSE4)
         {
-            Debug.Log("Not Collision!");
+            Debug.LogError("WRONG COLLISION");
             colliding = false;
             isCollisionDetectedRight = false;
+            spriteRenderer.color = wrongCollisionColor;
         }
 
         if(_collision.gameObject.tag == _dominoTagFALSE5)
         {
-            Debug.Log("Not Collision!");
+            Debug.LogError("WRONG COLLISION");
             colliding = false;
             isCollisionDetectedRight = false;
+            spriteRenderer.color = wrongCollisionColor;
         } 
 
         CollidingSound(); //hier moet ik dus hebben wanneer de colliding goed is dat die de position vastzet
@@ -89,6 +108,13 @@ public class DominoCollision : MonoBehaviour
         if(_collision.gameObject.tag == _dominoTagTRUE)
         {
             isCollisionDetectedRight = false;
+
+            if (colorChangeCoroutine != null)
+            {
+                StopCoroutine(colorChangeCoroutine);
+            }
+
+            colorChangeCoroutine = StartCoroutine(ChangeColorWithDelay(exitColor, delay));
         }
     }
 
@@ -109,5 +135,11 @@ public class DominoCollision : MonoBehaviour
             Debug.Log("Played");
         }
     }
-}
 
+    private IEnumerator ChangeColorWithDelay(Color targetColor, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        spriteRenderer.color = targetColor;
+    }
+}
