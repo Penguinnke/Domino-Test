401cdb0f58f30041ad7e4a897cb75d1419ca4d1b
Penguinnke <121119907+Penguinnke@users.noreply.github.com>
2023-10-03 22:47:43 +0200

    SceneChanger

--- Assets/Scripts/NotUsed/Domino/CopyAndEnableObject.cs
+++ Assets/Scripts/NotUsed/Domino/CopyAndEnableObject.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class CopyAndEnableObject : MonoBehaviour
 {
@@ -10,6 +11,21 @@ public class CopyAndEnableObject : MonoBehaviour
     public float maxX = 10f; // Maximum X-axis value
     public float minY = -5f; // Minimum Y-axis value
     public float maxY = 5f; // Maximum Y-axis value
+
+    private string savePath = "savedPositions.json"; // Path to the saved data file
+
+    private List<Vector3> savedPositions = new List<Vector3>();
+
+    private Transform objectContainer; // Reference to the ObjectContainer
+
+    void Start()
+    {
+        objectContainer = GameObject.Find("ObjectContainer").transform;
+        //objectContainer = new GameObject("ObjectContainer").transform;
+        DontDestroyOnLoad(objectContainer.gameObject); // Make the ObjectContainer persistent
+
+        LoadSavedPositions();
+    }
 
     void Update()
     {
@@ -24,7 +40,7 @@ public class CopyAndEnableObject : MonoBehaviour
         int randomIndex = Random.Range(0, objectsToCopy.Length); // Randomly select an index
         GameObject randomObject = objectsToCopy[randomIndex]; // Get the randomly selected object
 
-        GameObject copiedObject = Instantiate(randomObject);
+        GameObject copiedObject = Instantiate(randomObject, objectContainer);
         copiedObject.SetActive(true);
 
         float randomX = Random.Range(minX, maxX);
@@ -32,5 +48,49 @@ public class CopyAndEnableObject : MonoBehaviour
 
         float zPos = randomObject.transform.position.z;
         copiedObject.transform.position = new Vector3(randomX, randomY, zPos);
+
+        // Save the position of the copied object
+        SaveObjectPosition(copiedObject.transform.position);
+    }
+
+    private void SaveObjectPosition(Vector3 position)
+    {
+        savedPositions.Add(position);
+
+        // Serialize and save to disk
+        string json = JsonUtility.ToJson(savedPositions);
+        File.WriteAllText(savePath, json);
+
+        Debug.Log("Position saved: " + position);
+    }
+
+    private void LoadSavedPositions()
+    {
+        Debug.Log("LoadSavedPositions method called."); // Add this line for debugging
+
+        if (File.Exists(savePath))
+        {
+            string json = File.ReadAllText(savePath);
+            savedPositions = JsonUtility.FromJson<List<Vector3>>(json);
+
+            // Loop through saved positions and instantiate objects
+            foreach (var position in savedPositions)
+            {
+                int randomIndex = Random.Range(0, objectsToCopy.Length); // Randomly select an index
+                GameObject randomObject = objectsToCopy[randomIndex]; // Get the randomly selected object
+
+                GameObject copiedObject = Instantiate(randomObject, objectContainer);
+                copiedObject.SetActive(true);
+
+                float zPos = randomObject.transform.position.z;
+                copiedObject.transform.position = new Vector3(position.x, position.y, zPos);
+
+                Debug.Log("Position loaded: " + position);
+            }
+        }
+        else
+        {
+            Debug.Log("No saved positions found.");
+        }
     }
 }
